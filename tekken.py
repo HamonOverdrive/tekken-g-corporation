41b8cef268c39ebcc12904da06779bf0492980b7
rlee <robinleesuns13@gmail.com>
2018-02-23 21:03:36 -0800

    new csv images moved features to separate py file and changes in nav bar and footer

--- tekken.py
+++ tekken.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
-import pandas as pd
-from bs4 import BeautifulSoup
+from site_features import inject_frame_data
 
 app = Flask(__name__)
 
@@ -9,24 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+# frame data tables for each character
 @app.route('/frame-data/<string:character>')
 def frame_page(character):
-    # get data frame of character csv into html format object for jinja
-    df1 = pd.read_csv(fr'/home/rlee/PycharmProjects/tekken-g-corporation/static/char_csvs2/{character}1.csv')
-    df2 = pd.read_csv(fr'/home/rlee/PycharmProjects/tekken-g-corporation/static/char_csvs2/{character}2.csv')
-
-    # gets rid of Nan date with blanks have to use this so html does not show it
-    df1 = df1.fillna('')
-    df2 = df2.fillna('')
-
-    # inject data table with new class and border for custom css usage also can be done with other attributes like 'id'
-    frame_soup1 = BeautifulSoup(df1.to_html(index=False), "html.parser")
-    frame_soup1.find('table')['class'] = 'framedata_table'
-    frame_soup1.find('table')['border'] = '0'
-
-    frame_soup2 = BeautifulSoup(df2.to_html(index=False), "html.parser")
-    frame_soup2.find('table')['class'] = 'framedata_table'
-    frame_soup2.find('table')['border'] = '0'
+    frame_soup1, frame_soup2 = inject_frame_data(character)
 
     return render_template("char_frame_page.html", character=character, ctable1=frame_soup1, ctable2=frame_soup2)
 
