97503279a1a4d360f00ba8970642e9b2a61fde1b
Robin <robinleesuns13@gmail.com>
2018-04-17 20:38:43 -0700

    got rid of not longer used functionality

--- site_features.py
+++ site_features.py
@@ -2,6 +2,7 @@ import os, pandas as pd
 from bs4 import BeautifulSoup, Tag
 
 
+# how csv data can turn into html table https://stackoverflow.com/questions/44320329/converting-csv-to-html-table-in-python?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 def inject_frame_data(character):
     'Injects frame data tables to website'
 
@@ -28,39 +29,39 @@ def inject_frame_data(character):
 
 def customizeParsedHtmlTalbe(dataTable, tableNo):
 
-    #customize headers (for sorting arrows)
-    for thRow in dataTable.find_all('th'):
-
-        # Building this HTML to replace the <th></th>:
-        #<th class="move-table-th">
-        #    <p class="move-table-column-name"> + thRow.string + </p>
-        #    <span class="move-table-arrow-container">
-        #        <div class="arrow-up" onclick="sort(this)"></div>
-        #        <div class="arrow-down" onclick="sort(this)"></div>
-        #    </span>
-        #</th>
-
-        pTag = Tag(dataTable, name="p")
-        pTag['class'] = 'move-table-column-name'
-        pTag.string = thRow.string
-
-        spanTag = Tag(dataTable, name="span")
-        spanTag['class'] = 'move-table-arrow-container'
-
-        divUpTag = Tag(dataTable, name="div")
-        divUpTag['class'] = 'arrow-up'
-        divUpTag['onclick'] = 'sort(this)'
-
-        divDownTag = Tag(dataTable, name="div")
-        divDownTag['class'] = 'arrow-down'
-        divDownTag['onclick'] = 'sort(this)'
-
-        spanTag.insert(0, divUpTag)
-        spanTag.insert(1, divDownTag)
-
-        thRow.string = ''   #clean the old value
-        thRow.insert(0, pTag)
-        thRow.insert(1, spanTag)
+    # #customize headers (for sorting arrows)
+    # for thRow in dataTable.find_all('th'):
+    #
+    #     # Building this HTML to replace the <th></th>:
+    #     #<th class="move-table-th">
+    #     #    <p class="move-table-column-name"> + thRow.string + </p>
+    #     #    <span class="move-table-arrow-container">
+    #     #        <div class="arrow-up" onclick="sort(this)"></div>
+    #     #        <div class="arrow-down" onclick="sort(this)"></div>
+    #     #    </span>
+    #     #</th>
+    #
+    #     pTag = Tag(dataTable, name="p")
+    #     pTag['class'] = 'move-table-column-name'
+    #     pTag.string = thRow.string
+    #
+    #     spanTag = Tag(dataTable, name="span")
+    #     spanTag['class'] = 'move-table-arrow-container'
+    #
+    #     divUpTag = Tag(dataTable, name="div")
+    #     divUpTag['class'] = 'arrow-up'
+    #     divUpTag['onclick'] = 'sort(this)'
+    #
+    #     divDownTag = Tag(dataTable, name="div")
+    #     divDownTag['class'] = 'arrow-down'
+    #     divDownTag['onclick'] = 'sort(this)'
+    #
+    #     spanTag.insert(0, divUpTag)
+    #     spanTag.insert(1, divDownTag)
+    #
+    #     thRow.string = ''   #clean the old value
+    #     thRow.insert(0, pTag)
+    #     thRow.insert(1, spanTag)
 
 
     #add id (they should be different for all the tables), class and border to the table
